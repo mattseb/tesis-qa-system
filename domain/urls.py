7539411c193a3d21d5439829ae47a5c8a198c271
Mateo Martínez <61299044+mattseb@users.noreply.github.com>
2023-08-07 22:38:19 -0500

    Add files via upload

---
+++ domain/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from domain import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("subdomains/", views.get_subdomains, name='subdomains'),
+    path("domains/", views.get_search_concepts, name='domains'),
+    path("configurations/", views.modify_configurations, name='configurations'),
+    path("add_filter/<str:filter_value>/<str:filter_action>/", views.add_filter, name='add_filter'),
+]
